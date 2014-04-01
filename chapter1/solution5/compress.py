d4620c5a8bedfafc5618f192bc73446a01d6d6b1
rioshen <rioxshen@gmail.com>
2014-04-01 17:11:47 -0400

    solution 5

--- chapter1/solution5/compress.py
+++ chapter1/solution5/compress.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python
 
-def compress_str(str):
+__author__ = "Rio"
 
+def compress_str(string):
+    ''' Compress string where duplicated characters become number.
+
+    >> compress_str('a')
+    >> a
+
+    >> compress_str('abc')
+    >> a1b1c1
+
+    >> compress_str('aabb')
+    >> a2b2
+    '''
+    if not string:
+        return None
+
+    if len(string) == 1:
+        return string
+
+    last = ''
+    count = 1
+    result = []
+    for chr in string:
+        if last == chr:
+            count += 1
+        else:
+            if last:
+                result.append(last+str(count))
+            last = chr
+            count = 1
+
+    result.append(last+str(count))
+    return ''.join(result)
